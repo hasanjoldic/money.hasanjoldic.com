472ba13483c969004cb2e24e9103969abb3af18a
Hasan Joldic <hasan.joldic@enki.ba>
2022-09-12 18:32:43 +0000

    fix(openapi): use commonjs imports

--- packages/openapi/index.js
+++ packages/openapi/index.js
@@ -1,4 +1,4 @@
-import path from "path";
-import YAML from "yamljs";
+const path = require("path");
+const YAML = require("yamljs");
 
 export var v1 = YAML.load(path.resolve(__dirname, "./versions/v1.yaml"));
